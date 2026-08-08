b47bdff842890eb3e5bf1f86b7bb860ea26d44d4
remasv999-del <remasv999@gmail.com>
2026-08-08 03:43:34 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="site-footer">
-    <div class="container footer-content">
-        <p>&copy; <?= date('Y') ?> Campus Events Hub.</p>
-        <p>Developed for the IT361 Web Programming Project.</p>
-    </div>
-</footer>
-</body>
-</html>
